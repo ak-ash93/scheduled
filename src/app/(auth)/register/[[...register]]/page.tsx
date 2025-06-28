80658264f959375e9e38bc97444aa04dd2128540
ak-ash93 <ash9793ak@gmail.com>
2025-06-28 09:55:15 +0800

    register page completed

--- src/app/(auth)/register/[[...register]]/page.tsx
+++ src/app/(auth)/register/[[...register]]/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Image from "next/image";
+import { calendar } from "../../../../../public/assets";
+import { SignUp } from "@clerk/nextjs";
 
-const page = () => {
-  return <div>page</div>;
+const RegisterPage = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center p-6 gap-8 ">
+        <h1 className="text-3xl xl:mt-20 font-extrabold text-gray-600 tracking-wider">
+          Skeduled
+        </h1>
+        <Image
+          src={calendar}
+          width={200}
+          height={200}
+          alt="scheduled logo image"
+        />
+        <div className="mt-5">
+          <SignUp />
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default page;
+export default RegisterPage;
